660bd172951aeab3ce96dde036f0340558cd23af
Tixiao Shan <tixiao.shan@gmail.com>
2019-05-05 10:08:58 -0400

    Update install-Software.sh

--- install-Software.sh
+++ install-Software.sh
@@ -50,7 +50,7 @@ sudo gdebi sublime.deb
 echo "${green}Install gtsam ${reset}"
 wget -O ~/Downloads/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.0-alpha2.zip
 cd ~/Downloads/ && unzip gtsam.zip -d ~/Downloads/
-cd ~/Downloads/gtsam-4.0.0-alpha2
+cd ~/Downloads/gtsam-4.0.0-alpha2/
 mkdir build && cd build
 cmake ..
 sudo make install -j4
