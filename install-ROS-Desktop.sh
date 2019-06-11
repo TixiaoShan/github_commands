f00c48e050c80821ee20b31ddc8a9f4a98fd42ef
Tixiao Shan <tixiao.shan@gmail.com>
2019-06-11 19:40:48 -0400

    Update install-ROS-Desktop.sh

--- install-ROS-Desktop.sh
+++ install-ROS-Desktop.sh
@@ -9,7 +9,7 @@ echo "${green}Begin to install ...${reset}"
 # ROS Kinetic
 echo "${green}Begin to install ROS Kinetic...${reset}"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y ros-kinetic-desktop-full
 # Initialize rosdep
