d36fcdab91b039f3c011029970c98e104b758dba
Tixiao Shan <tixiao.shan@gmail.com>
2019-09-16 15:38:41 -0400

    Update and rename install-RealSense-SDK.sh to install-RealSense.sh

--- install-RealSense.sh
+++ install-RealSense.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo apt-get install -y ros-kinetic-librealsense2
+sudo apt-get install -y ros-kinetic-realsense2-camera
+
 # Add public key
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key
 
