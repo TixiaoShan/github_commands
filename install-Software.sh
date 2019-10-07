837d7a63973b835f4d7ba25e03cf8095e0dcd40f
Tixiao Shan <tixiao.shan@gmail.com>
2019-10-07 15:12:36 -0400

    Update install-Software.sh

--- install-Software.sh
+++ install-Software.sh
@@ -44,7 +44,7 @@ wget -O ~/Downloads/dropbox.deb "https://www.dropbox.com/download?dl=packages/ub
 sudo gdebi dropbox.deb
 wget -O ~/Downloads/chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo gdebi chrome.deb
-wget -O ~/Downloads/sublime.deb "https://download.sublimetext.com/sublime-text_build-3176_amd64.deb"
+wget -O ~/Downloads/sublime.deb "https://download.sublimetext.com/sublime-text_build-3207_amd64.deb"
 sudo gdebi sublime.deb
 
 #########################################################
@@ -70,17 +70,7 @@ sudo make install -j4
 
 
 #########################################################
-#sgbteam
-#Single User License
-#EA7E-1153259
-#8891CBB9 F1513E4F 1A3405C1 A865D53F
-#115F202E 7B91AB2D 0D2A40ED 352B269B
-#76E84F0B CD69BFC7 59F2DFEF E267328F
-#215652A3 E88F9D8F 4C38E3BA 5B2DAAE4
-#969624E7 DC9CD4D5 717FB40C 1B9738CF
-#20B3C4F1 E917B5B3 87C38D9C ACCE7DD8
-#5F7EF854 86B9743C FADC04AA FB0DA5C0
-#F913BE58 42FEA319 F954EFDD AE881E0B
+# https://gist.github.com/dinhchi27/80d49460fd4529e21e6b60cc77c79b54
 
 # Just go to Preferences -> Settings-User and add there: "update_check": false,
 
