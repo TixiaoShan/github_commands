fb4d4905fab51f4265677c928f017fb7bd1ed3f6
Tixiao Shan <tixiao.shan@gmail.com>
2020-01-22 14:56:31 -0500

    Add files via upload

---
+++ install-Ceres.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# http://ceres-solver.org/installation.html
+cd ~
+git clone https://ceres-solver.googlesource.com/ceres-solver
+# sudo apt-get install cmake
+sudo apt-get install libgoogle-glog-dev
+sudo apt-get install libatlas-base-dev
+# sudo apt-get install libeigen3-dev
+
+cd ceres-solver
+mkdir ceres-bin
+cd ceres-bin
+cmake ../../ceres-solver
+make -j4
+make test
+sudo make install
