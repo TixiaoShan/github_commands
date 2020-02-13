b4fd544b2eaae5e612c9b2b7227f64e19aabef79
Tixiao Shan <tixiao.shan@gmail.com>
2020-02-12 19:34:18 -0500

    Update install-Ceres.sh

--- install-Ceres.sh
+++ install-Ceres.sh
@@ -5,12 +5,9 @@ git clone https://ceres-solver.googlesource.com/ceres-solver
 # sudo apt-get install cmake
 sudo apt-get install libgoogle-glog-dev
 sudo apt-get install libatlas-base-dev
-# sudo apt-get install libeigen3-dev
+sudo apt-get install libeigen3-dev
 
 cd ceres-solver
-mkdir ceres-bin
-cd ceres-bin
-cmake ../../ceres-solver
-make -j4
-make test
-sudo make install
+mkdir ceres-bin && cd ceres-bin
+cmake ..
+sudo make install -j4
