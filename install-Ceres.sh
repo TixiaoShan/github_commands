33f4f1fae15202ea974932e35d46fc558e75cb84
Tixiao Shan <tixiao.shan@gmail.com>
2020-02-19 08:52:18 -0500

    Update install-Ceres.sh

--- install-Ceres.sh
+++ install-Ceres.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # http://ceres-solver.org/installation.html
-cd ~
+cd ~/Downloads
 git clone https://ceres-solver.googlesource.com/ceres-solver
-# sudo apt-get install cmake
-sudo apt-get install libgoogle-glog-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libeigen3-dev
 
-cd ceres-solver
+# Install eigen
+wget -O ~/Downloads/eigen.deb "http://ftp.br.debian.org/debian/pool/main/e/eigen3/libeigen3-dev_3.3.7-1_all.deb"
+cd ~/Downloads && sudo gdebi eigen.deb
+
+# sudo apt-get install cmake
+sudo apt-get install -y libgoogle-glog-dev
+sudo apt-get install -y libatlas-base-dev
+
+# Compile Ceres
+cd ~/Downloads/ceres-solver
 mkdir ceres-bin && cd ceres-bin
 cmake ..
 sudo make install -j4
