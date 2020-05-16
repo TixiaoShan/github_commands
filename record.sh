e6ed2a79502874529466ef6a01881345bca6bffb
Tixiao Shan <tixiao.shan@gmail.com>
2020-05-15 23:18:58 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -1,4 +1,4 @@
 cd ~/Downloads
-mkdir $(date +%Y%m%d_%H%M)
-cd $(date +%Y%m%d_%H%M)
-rosbag record --split --duration=10m /tf /tf_static /laser_cloud_less_flat /laser_cloud_less_sharp /velodyne_cloud_registered /ground_cloud /segmented_cloud_pure
+folder=$(date +%Y%m%d_%H%M%S)
+mkdir $folder && cd $folder
+rosbag record --split --duration=1m /points_raw /imu_raw /camera/image_raw
