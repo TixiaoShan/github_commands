6ba267ca324485444c352adbd80180a4950100c5
Tixiao Shan <tixiao.shan@gmail.com>
2020-06-11 10:11:48 -0400

    Update install-CUDNN.sh

--- install-CUDNN.sh
+++ install-CUDNN.sh
@@ -10,4 +10,7 @@ sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h
 sudo chmod a+r /usr/local/cuda/lib64/libcudnn*
-echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+
+# ! add the following lines to your bashrc file
+# export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
