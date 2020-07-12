130f2ab7b7192d1bda77cd248f46815341e4fb94
Tixiao Shan <tixiao.shan@gmail.com>
2020-07-12 09:38:43 -0400

    Update mp4.sh

--- mp4.sh
+++ mp4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ffmpeg -i input.ogv \
-       -c:v libx264 -preset veryslow -crf 22 \
+       -c:v libx264 -preset medium -crf 22 \
        -c:a libmp3lame -qscale:a 2 -ac 2 -ar 44100 \
        output.mp4
