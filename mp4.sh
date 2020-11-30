b6eb0e11a418c4454d2af2b83348e98d81b430f5
Tixiao Shan <tixiao.shan@gmail.com>
2020-11-30 08:30:29 -0500

    Update mp4.sh

--- mp4.sh
+++ mp4.sh
@@ -3,3 +3,11 @@ ffmpeg -i input.ogv \
        -c:v libx264 -preset medium -crf 22 \
        -c:a libmp3lame -qscale:a 2 -ac 2 -ar 44100 \
        output.mp4
+
+# convert batch
+# for i in /path/to/*.ogv;
+#   do name=`echo "$i" | cut -d'.' -f1`
+#   echo "$name"
+#   ffmpeg -i "$i" "${name}.mp4" \
+#        -c:v libx264 -preset medium -crf 22
+# done
