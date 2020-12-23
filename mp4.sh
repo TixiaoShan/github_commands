d9f1aa8aaa978ac0d92ad9259ca2a76b4c697aae
Tixiao Shan <tixiao.shan@gmail.com>
2020-12-23 18:14:24 -0500

    compatibility with quicktime

--- mp4.sh
+++ mp4.sh
@@ -2,6 +2,7 @@
 ffmpeg -i input.ogv \
        -c:v libx264 -preset medium -crf 22 \
        -c:a libmp3lame -qscale:a 2 -ac 2 -ar 44100 \
+       -pix_fmt yuv420p \
        output.mp4
 
 # convert batch
@@ -9,5 +10,5 @@ ffmpeg -i input.ogv \
 #   do name=`echo "$i" | cut -d'.' -f1`
 #   echo "$name"
 #   ffmpeg -i "$i" "${name}.mp4" \
-#        -c:v libx264 -preset medium -crf 22
+#        -c:v libx264 -pix_fmt yuv420p -preset medium -crf 22
 # done
