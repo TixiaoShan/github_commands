b96070ebbe89a7be009594d2783d817ac9dc7748
Tixiao Shan <tixiao.shan@gmail.com>
2021-03-05 16:24:21 -0500

    Update jpg_to_mp4.sh

--- jpg_to_mp4.sh
+++ jpg_to_mp4.sh
@@ -7,5 +7,6 @@ ffmpeg -r ${frameRate} \
        -i ${jpgName} \
        -vcodec libx264 \
        -crf 20 \
+       -pix_fmt yuv420p \
        -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
        ${mp4Name}
